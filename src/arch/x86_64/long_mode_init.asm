aaad9db2e88099c27ad6a5314c80e340b3af4485
Mathew Robinson <mathew.robinson@kroger.com>
2016-06-03 11:32:29 -0400

    changed okay position

--- src/arch/x86_64/long_mode_init.asm
+++ src/arch/x86_64/long_mode_init.asm
@@ -5,6 +5,6 @@ bits 64
 long_mode_start:
   ; Set up a 64 bit version of eax
   mov rax, 0x2f592f412f4b2f4f
-  mov qword [0xb8022], rax      ; Print OKAY to the screen
+  mov qword [0xb8000], rax      ; Print OKAY to the screen
 
   hlt
