0b4565ec276005c06fc89314e5ae9e78825755a3
Mathew Robinson <mathew.robinson@kroger.com>
2016-06-03 15:37:07 -0400

    print hello world! to middle of screen

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,19 @@ extern crate rlibc;
 
 #[no_mangle]
 pub extern fn rust_main() {
-    let x = ["Hello", "World", "!"];
+    // ATTENTION: we have a very small stack and no guard page
+    let hello = b"Hello World!";
+    let color_byte = 0x0F;
+
+    let mut hello_colored = [color_byte; 24];
+    for (i, char_byte) in hello.into_iter().enumerate() {
+        hello_colored[i*2] = *char_byte;
+    }
+
+    let buffer_ptr = (0xb8000 + 1988) as *mut _;
+    unsafe { *buffer_ptr = hello_colored }
+
+    loop{}
 }
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
